70ba2024be54850b6c124a848d3036195847d7a7
Munteanu Silviu-Cristian <97188966+munteanusil@users.noreply.github.com>
2025-12-24 00:10:20 -0800

    Add Store Procedure #1
    Procedure for placing a complete order

---
+++ 04_StoreProcedure.sql
@@ -0,0 +1,66 @@
+﻿USE RetailStoreDB;
+GO
+
+
+CREATE OR ALTER PROCEDURE sp_PlaceOrder
+    @CustomerID INT,
+    @ProductID INT,
+    @Quantity INT
+AS
+BEGIN
+  
+    SET NOCOUNT ON;
+    
+    DECLARE @AvailableStock INT;
+    DECLARE @ProductPrice DECIMAL(10,2);
+    DECLARE @InsertedOrderID INT;
+
+    SELECT @AvailableStock = StockQuantity, @ProductPrice = Price 
+    FROM Products WHERE ProductID = @ProductID;
+
+    IF @AvailableStock IS NULL
+    BEGIN
+        RAISERROR('Eroare: Produsul nu există.', 16, 1);
+        RETURN;
+    END
+
+    IF @AvailableStock < @Quantity
+    BEGIN
+        RAISERROR('Eroare: Stoc insuficient!', 16, 1);
+        RETURN;
+    END
+
+   
+    BEGIN TRANSACTION;
+
+    BEGIN TRY
+     
+        UPDATE Products 
+        SET StockQuantity = StockQuantity - @Quantity 
+        WHERE ProductID = @ProductID;
+
+        
+        INSERT INTO Orders (CustomerID, OrderDate, TotalAmount)
+        VALUES (@CustomerID, GETDATE(), @ProductPrice * @Quantity);
+
+        
+        SET @InsertedOrderID = SCOPE_IDENTITY();
+
+        
+        INSERT INTO OrderDetails (OrderID, ProductID, Quantity, UnitPrice)
+        VALUES (@InsertedOrderID, @ProductID, @Quantity, @ProductPrice);
+
+       
+        COMMIT TRANSACTION;
+        PRINT 'Comanda a fost procesată cu succes! Order ID: ' + CAST(@InsertedOrderID AS VARCHAR);
+        
+    END TRY
+    BEGIN CATCH
+       
+        ROLLBACK TRANSACTION;
+        
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR(@ErrorMessage, 16, 1);
+    END CATCH
+END;
+GO
